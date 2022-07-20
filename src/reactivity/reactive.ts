f1f263d6a692a3e8ae7d9937bc933ebb8eb52869
linzheng <alidlg9045@126.com>
2022-07-20 23:51:25 +0800

    实现isReactive和isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers"
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive',
+  IS_READONLY = '__v_isReadonly',
+}
+
 export function reactive(raw) {
   return createActiceObject(raw, mutableHandlers)
 }
@@ -7,6 +12,15 @@ export function reactive(raw) {
 export function readonly(raw) {
   return createActiceObject(raw, readonlyHandlers)
 }
+
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
+};
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY]
+}
+
 
 function createActiceObject(raw: any, baseHandlers) {
   return new Proxy(raw, baseHandlers)
