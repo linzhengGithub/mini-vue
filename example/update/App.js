abfd06af34da634771f4f1241e75201e28a2ac43
linzheng <alidlg9045@126.com>
2022-08-16 23:52:59 +0800

    编写更新 element 流程中的 props 的测试案例

--- example/update/App.js
+++ example/update/App.js
@@ -7,9 +7,35 @@ export const App = {
     const onClick = () => {
       count.value++
     }
+
+    const props = ref({
+      foo: 'foo',
+      bar: 'bar',
+    })
+
+     const onChangePropsDemo1 = () => {
+      // foo 值修改了
+      props.value.foo = 'new-foo'
+    }
+    const onChangePropsDemo2 = () => {
+      // foo 变成了 undefined / null
+      props.value.foo = undefined
+    }
+    const onChangePropsDemo3 = () => {
+      // 删除了 bar
+      props.value = {
+        foo: 'foo'
+      }
+    }
+
+
     return {
       count,
-      onClick
+      onClick,
+      props,
+      onChangePropsDemo1,
+      onChangePropsDemo2,
+      onChangePropsDemo3
     }
   },
   render() {
@@ -20,7 +46,10 @@ export const App = {
       },
       [
         h('div', {}, 'count:' + this.count), // 收集依赖
-        h('button', { onClick: this.onClick }, 'click')
+        h('button', { onClick: this.onClick }, 'click'),
+        h('button', { onClick: this.onChangePropsDemo1 }, 'click-change-props-demo1'),
+        h('button', { onClick: this.onChangePropsDemo2 }, 'click-change-props-demo2'),
+        h('button', { onClick: this.onChangePropsDemo3 }, 'click-change-props-demo3'),
       ]
     )
   }
