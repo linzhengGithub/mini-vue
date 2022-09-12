90def79a8e6b98ac8a9a0c9e3921e1548960ab3f
linzheng <alidlg9045@126.com>
2022-09-12 17:06:52 +0800

    实现 解析插值 功能

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,13 +1,57 @@
+import { NodeTypes } from "./ast"
+
 export function baseParse(content: string) {
+  const context = createParseContext(content)
+  return createRoot(parseChildren(context))
+}
+
+function parseChildren(context: any) {
+  const nodes: any = []
+
+  let node
+  if (context.source.startsWith('{{')) {
+    node = parseInterpolation(context)
+  }
+
+  nodes.push(node)
+  return nodes
+}
+
+function parseInterpolation(context) {
+  const openDelimiter = '{{'
+  const closeDelimiter = '}}'
+
+  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length)
+
+  advanceBy(context, openDelimiter.length)
+  const rawContentLength = closeIndex - openDelimiter.length
+
+  const rawContent = context.source.slice(0, rawContentLength)
+  const content = rawContent.trim()
+
+  advanceBy(context, rawContentLength + closeDelimiter.length)
+
   return {
-    children: [
-      {
-        type: 'interpolation',
-        content: {
-          type: 'simple_expression',
-          content: 'message'
-        }
-      }
-    ]
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content
+    }
+  }
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length)
+}
+
+function createRoot(children): any {
+  return {
+    children
+  }
+}
+
+function createParseContext(content: string): any {
+  return {
+    source: content
   }
 }
