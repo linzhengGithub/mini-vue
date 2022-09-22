58c59062eb5f7ac1d506272c2efc323e93f8f2f7
linzheng <alidlg9045@126.com>
2022-09-23 00:13:27 +0800

    实现 transform 功能（通过外部自定义函数传入）

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,24 +1,41 @@
-import { NodeTypes } from "./ast";
-
-
-export function transform(root) {
+export function transform(root, options) {
+  const context = createTransformContext(root, options)
   // 1. 遍历 - 深度优先搜索
-  traverseNode(root)
+  traverseNode(root, context)
   // 2. 修改 text content
 }
 
-function traverseNode(node: any) {
-  console.log(node);
-  const children = node.children
-  // 先粗暴的通过单测
-  if(node.type === NodeTypes.TEXT) {
-    node.content = node.content + 'mini-vue'
+function createTransformContext(root: any, options: any): any {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
   }
 
+  return context
+}
+
+function traverseNode(node: any, context) {
+  // if (node.type === NodeTypes.TEXT) {
+  //   node.content = node.content + 'mini-vue'
+  // }
+  // 将上面代码 通过 plugin 的方式执行
+  const { nodeTransforms } = context
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  console.log(node);
+
+  traverseChildren(node, context);
+}
+
+function traverseChildren(node, context) {
+  const children = node.children
   if (children) {
     for (let i = 0; i < children.length; i++) {
-      const node = children[i]
-      traverseNode(node)
+      const node = children[i];
+      traverseNode(node, context);
     }
   }
 }
