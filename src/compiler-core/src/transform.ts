1d549185639989239b662f52fde352b4ea3026b6
linzheng <alidlg9045@126.com>
2022-09-22 23:45:23 +0800

    简单通过 transform 功能单测

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,5 +1,24 @@
+import { NodeTypes } from "./ast";
 
 
 export function transform(root) {
-  
+  // 1. 遍历 - 深度优先搜索
+  traverseNode(root)
+  // 2. 修改 text content
+}
+
+function traverseNode(node: any) {
+  console.log(node);
+  const children = node.children
+  // 先粗暴的通过单测
+  if(node.type === NodeTypes.TEXT) {
+    node.content = node.content + 'mini-vue'
+  }
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node)
+    }
+  }
 }
